c1917450e866ef62733ae4a1bfab7aa99d3dcf1c
christianmesaque <christianmacedo110@gmail.com>
2023-04-04 16:52:02 -0300

    Aula 04

---
+++ Aula04/src/Caneta.java
@@ -0,0 +1,47 @@
+public class Caneta {
+    public String modelo;
+    private float ponta;
+    private String cor;
+    private boolean tampada;
+
+    // Esté é o método construtor
+    public Caneta(String m, String c, float p) {
+        this.setModelo(m);
+        this.cor = c;
+        this.setPonta(p);
+        this.tampar();
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String m) {
+        this.modelo = m;
+    }
+
+    public float getPonta() {
+        return this.ponta;
+    }
+
+    public void setPonta(float p) {
+        this.ponta = p;
+    }
+
+    public void tampar() {
+        this.tampada = true;
+    }
+
+    public void destampar() {
+        this.tampada = false;
+    }
+
+    public void status() {
+        System.out.println("SOBRE A CANETA: ");
+        System.out.println("MODELO: " + this.getModelo());
+        System.out.println("PONTA: " + this.getPonta());
+        System.out.println("COR: " + this.cor);
+        System.out.println("TAMPADA: " + this.tampada);
+    }
+
+}
